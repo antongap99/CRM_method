0e6d84613f212f25a21ad3f65edc7b6b1a252ca1
Anton <anton.gap99@gmail.com>
2022-06-12 21:08:45 +0300

    add modal.js

---
+++ script.js/modal.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal_top .modal__title');
+const modalForm = document.querySelector('.modal .modal__form');
+const discountCheckBox = modalForm.querySelector('#discount');
+const discountInput = modalForm.querySelector('.modal__input.modal__input_discount')
+console.log('discountInput: ', discountInput);
+console.log('discountCheckBox: ', discountCheckBox);
+console.log('modalForm: ', modalForm);
+console.log('modalTitle: ', modalTitle);
